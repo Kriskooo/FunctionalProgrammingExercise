8df422e592186ffa1968b8bc3525ebe32f24ebcd
Kriskooo <kris.danielov@gmail.com>
2021-02-02 12:37:50 +0200

    FunctionalProgrammingExercise

---
+++ FunctionalProgrammingExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace ActionPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> names = x => Console.WriteLine($"{string.Join(Environment.NewLine, x)}");
+            Console.ReadLine()
+                        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                        .ToList()
+                        .ForEach(names);
+        }
+    }
+}
---
+++ KnightsOfHonor/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace KnightsOfHonor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> print = name => Console.WriteLine($"Sir {name}");
+
+            Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList().ForEach(print);
+        }
+    }
+}
+    
